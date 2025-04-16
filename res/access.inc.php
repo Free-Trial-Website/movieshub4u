7eb4a9e1950fa1baaa7460fc35f584a62bcbcb9b
Free-Trial-Website <kingopkingop010@gmail.com>
2025-04-16 15:27:39 +0500

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'f04mfwfl';
+$imSettings['access']['users'] = array(
+	'kingoperaop1001@gmail.com' => array(
+		'groups' => array('8xl2qc82'),
+		'id' => 'b7tetsvz',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => '6Ep5fO4Ni',
+		'email' => 'kingoperaop1001@gmail.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('b7tetsvz');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
